b7c5f3e91c88a96b2a8b850b3e9e5b1ebfd78f82
ksb0511 <soobin796@gmail.com>
2022-07-10 16:53:17 +0900

    first commit

---
+++ app/src/main/java/com/ksb/compose_stduy/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.ksb.compose_stduy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ksb.compose_stduy.ui.theme.ComposeStduyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeStduyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+// 뷰
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+// 미리보기
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeStduyTheme {
+        Greeting("Android")
+    }
+}
